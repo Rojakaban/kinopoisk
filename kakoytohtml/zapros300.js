b4a04d3f3ed99b357f0cbb5d5db30962f180c971
Banan <cherepmn@mail.ru>
2023-02-11 17:02:05 +0500

    Сохранить файлы

---
+++ kakoytohtml/zapros300.js
@@ -0,0 +1,53 @@
+const postsDiv = document.getElementById('posts');
+
+async function getPosts() {
+    try {
+        const kinopoiskId = window.location.href.split('id=')[1];
+        const url= `https://kinopoiskapiunofficial.tech/api/v2.2/films/${kinopoiskId}`;
+        const response = await fetch(url, {
+        headers: {
+            'X-API-KEY': 'a6914d44-db6f-401e-96d4-9e84000a7075',
+            'Content-Type': 'application/json',
+        } 
+    })
+    const post = await response.json();
+    showPosts(post);
+    }
+    catch(error) {
+        console.error(error);
+        alert('Возникла ошибка. Не смогли получить фильмы.')
+    }
+}
+const button0 = document.querySelector('#dobavil')
+button0.onclick = function() {
+        console.log('djfd;f')
+        window.location.href = './zapros2.html'
+    }
+function showPosts(post) {
+
+
+
+        postsDiv.innerHTML += `
+        
+        <div style="width: 25%; margin-bottom: 20px; position: relative;">
+            <div><img src=${post.posterUrl} alt="" width="500" height="600"/>
+            style="position: absolute; top: 20px; left: 20px; color: green; text-shadow: black 1px 1px 0, black -1px -1px 0, black -1px 1px 0, black 1px -1px 0; font: 25px Arial, sans-serif">
+                ${post.ratingKinopoisk}
+            </div>
+            <p style="font-weight: bold; width: 300px; color: white; padding-left: 10px; text-shadow: black 1px 1px 0, black -1px -1px 0, black -1px 1px 0, black 1px -1px 0;
+            font: 25px Arial, sans-serif ">${post.nameRu}</p>
+            <p style="font-weight: bold; width: 200px; color: white; padding-left: 10px; text-shadow: black 1px 1px 0, black -1px -1px 0, black -1px 1px 0, black 1px -1px 0;
+            font: 17px Arial, sans-serif">Дата выхода: ${post.year}</p>
+            <p style="font-weight: bold; width: 200px; color: white; padding-left: 10px; text-shadow: black 1px 1px 0, black -1px -1px 0, black -1px 1px 0, black 1px -1px 0;
+            font: 17px Arial, sans-serif">Жанр: ${post.genres[0].genre}</p>
+            <p style="font-weight: bold; width: 200px; color: white; padding-left: 10px; text-shadow: black 1px 1px 0, black -1px -1px 0, black -1px 1px 0, black 1px -1px 0;
+            font: 17px Arial, sans-serif">Страна: ${post.countries[0] ? post.countries[0].country : ''}</>
+        </div>
+        
+        `
+   
+    
+}
+
+
+getPosts();
