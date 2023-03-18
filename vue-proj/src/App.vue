81c5a77e8a2616df9b928843185f56af14e9cebe
Banan <cherepmn@mail.ru>
2023-03-18 17:04:55 +0500

    нек

--- vue-proj/src/App.vue
+++ vue-proj/src/App.vue
@@ -1,16 +1,7 @@
 <template>
   <div id="app">
     <div class="form">
-    <!-- <div>
-    Количество лайков: {{ likes }}
-    </div>
-    <div>
-    Количество дизлайков: {{ dislikes }}
-    </div>
-    <div>
-      <button class="like" @click="addLike">Лайк</button>
-      <button class="dislike" @click="addDislike">Дизлайк</button>
-    </div> -->
+
     
     <h1>Создание поста</h1>
     
@@ -22,9 +13,6 @@
       >
     </div>
     
-    <!-- <div>
-    <button class="cp">Содержание поста</button>
-    </div> -->
     <div>
       <input
         v-model="text"
@@ -34,12 +22,14 @@
     </div>
     
     <div>
-    <button @click="addPost">Добавить пост</button>
+      <button @click="addPost">Добавить пост</button>
     </div>
     
     <div class="posts">
       <h2>Список постов</h2>
-      <div
+      
+      <post-list></post-list>
+      <!-- <div
         v-for="(post, index) in posts"
         :key="index"
         class="post"
@@ -54,23 +44,22 @@
       <div>
         <button style="bo" @click="deletePost(index)">Удалить</button>
       </div>
-    
+     -->
     </div>
     </div>
     </div>
-    </div>
-  </div>
 </template>
 
 <script>
+import PostList from './components/PostList.vue';
 export default {
+  components: { PostList },
   name: 'App',
   data() {
     return {
       title: '',
       text: '',
-      // likes: 0,
-      // dislikes: 0,
+
       posts: []
     };
   },
@@ -105,14 +94,5 @@ export default {
   margin-top: 60px;
   font-size: 32px;
 }
-/* .like{
-  background: green;
-  color: white;
-  cursor: pointer;
-}
-.dislike{
-  background: red;
-  color: white;
-  cursor: pointer;
-} */
+
 </style>
---
+++ vue-proj/src/components/PostList.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+<div
+        v-for="(post, index) in posts"
+        :key="index"
+        class="post"
+      >
+      <div style="display: flex; justify: space-between; align-items: center;">
+      
+      <div style="width: 90%;">
+        <h4>Название поста: {{ post.title }}</h4> <br>  
+        <p><strong>Содержание поста:</strong> {{ post.text }}</p>
+      </div>
+      
+      <div>
+        <button style="bo" @click="deletePost(index)">Удалить</button>
+      </div>
+    </div>
+</div>
+</div>
+    
+</template>
+
+<script>
+import PostList from '@/components/PostList.vue'
+
+export default {
+    name: 'App',
+    comments: {
+        PostList,
+    },
+    props: {
+        posts: {
+            type: Array,
+            default() {
+                return []
+            },
+            required: true,
+        },
+    },
+    
+}
+</script>
+
+<style> 
+
+</style>
